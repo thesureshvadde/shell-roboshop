da5c8ccaa938089710b9f6026e924a2f77434c4c
thesureshvadde <vaddesureshvarma@gmail.com>
2023-11-25 12:14:51 +0530

    mongo

--- common.sh
+++ common.sh
@@ -1,1 +1,17 @@
 codedir=$(pwd)
+
+logfile=/tmp/roboshop.log
+
+heading(){
+    echo -e "\e[31m $1 \e[0m"
+}
+
+status() {
+    if [ $1 eq 0 ]
+    then
+        echo "success"
+    else
+        echo "failure"
+        exit 1
+    if
+}
--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,26 @@
 source common.sh
-cp ${codedir}/configs/mongo.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org -y
-systemctl enable mongod
-systemctl start mongod
-echo "editing"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-echo "editing done"
-systemctl restart mongod
+rm -rf ${logfile}
+
+heading "coping mongo.repo file"
+cp ${codedir}/configs/mongo.repo /etc/yum.repos.d/mongo.repo &>> ${logfile}
+status $?
+
+heading "installing mongodb"
+yum install mongodb-org -y &>> ${logfile}
+status $?
+
+heading "enabling mongodb"
+systemctl enable mongod &>> ${logfile}
+status $?
+
+heading "starting mongodb"
+systemctl start mongod &>> ${logfile}
+status $?
+
+heading "editing mongodb listening address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> ${logfile}
+status $?
+
+heading "restarting mongodb"
+systemctl restart mongod &>> ${logfile}
+status $?

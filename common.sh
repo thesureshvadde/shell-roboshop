4d443d3dde33dd56bb3f8ed7c6c1accbdc385623
thesureshvadde <vaddesureshvarma@gmail.com>
2023-11-25 12:24:11 +0530

    mongo

--- common.sh
+++ common.sh
@@ -3,15 +3,15 @@ codedir=$(pwd)
 logfile=/tmp/roboshop.log
 
 heading(){
-    echo -e "\e[31m $1 \e[0m"
+    echo -e "\e[33m $1 \e[0m"
 }
 
 status(){
     if [ $1 -eq 0 ]
     then
-        echo "success"
+        echo -e "\e[32m success \e[0m"
     else
-        echo "failure"
+        echo "\e[31m failure \e[0m"
         exit 1
     fi
 }
